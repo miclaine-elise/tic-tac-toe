de1af7a7604bf2184b986b67a2d8906b1f69fa09
Miclaine Emtman <clainie@gmail.com>
2024-02-05 15:49:34 +0000

    Add addMarker object to Gameboard

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,19 +2,22 @@
 function Gameboard() {
     const rows = 3;
     const columns = 3;
-    const board = [];
+    let board = [];
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
             board[i].push("");
         }
     }
-    return board;
+    const getBoard = () => board;
+    const addMarker = (row, column, marker) => board[row][column] = marker;
+    return { getBoard, addMarker };
+
 }
 
 function GameController() {
     const board = Gameboard();
-    console.table(board);
+    console.table(board.getBoard());
     const players = [
         {
             name: "Player One",
@@ -29,28 +32,36 @@ function GameController() {
         console.log(currentPlayer.name + "'s turn");
         column = prompt("Enter which column to place your marker.");
         row = prompt("Enter which row to place your marker.");
-        board[row - 1][column - 1] = currentPlayer.marker;
-        console.table(board);
+        board.addMarker(row - 1, column - 1, getCurrentPlayer().marker);
+        console.table(board.getBoard());
     }
     const switchPlayerTurn = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     };
-    function checkForWinner(currentPlayer) {
-        checkVertical(currentPlayer);
-        function checkVertical(currentPlayer) {
-            if (board[0][0] === currentPlayer.marker && board[1][0] === currentPlayer.marker && board[2][0] === currentPlayer.marker) {
-                console.log("Gameover." + currentPlayer.name + "wins");
-                gameover = true;
+    function checkForWinner() {
+        const currentMarker = getCurrentPlayer().marker;
+        checkRows();
+        function checkMarkers(marker) {
+            return marker === currentMarker;
+        }
+        function checkRows() {
+            for (let row = 0; row < 3; row++) {
+                const boardRow = board.getBoard()[row];
+                if (boardRow.every(checkMarkers)) {
+                    console.log("gameover");
+                    gameover = true;
+                }
             }
+
         }
     }
     let gameover = false;
     const getCurrentPlayer = () => currentPlayer;
     while (gameover == false) {
         playTurn(currentPlayer);
-        checkForWinner(currentPlayer);
+        checkForWinner();
         switchPlayerTurn();
     }
-
+    return { currentPlayer }
 }
 GameController();
