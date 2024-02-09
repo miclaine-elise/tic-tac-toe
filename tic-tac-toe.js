ce0fd3e6d22ef78ceb31f243d34914134c491296
Miclaine Emtman <clainie@gmail.com>
2024-02-09 11:00:50 +0000

    Remove console log statements

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,16 +3,11 @@ function Gameboard() {
     const rows = 3;
     const columns = 3;
     let board = [];
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push("");
-        }
-    }
     const getBoard = () => board;
     const addMarker = (row, column, marker) => board[row][column] = marker;
-    const deleteBoard = () => board = [["", "", ""], ["", "", ""], ["", "", ""]];
-    return { getBoard, addMarker, deleteBoard };
+    const createBoard = () => board = [["", "", ""], ["", "", ""], ["", "", ""]];
+    createBoard();
+    return { getBoard, addMarker, createBoard };
 
 }
 
@@ -26,15 +21,15 @@ function GameController() {
             name: "Player Two",
             marker: "O"
         }];
-    let currentPlayer = players[0]; //Initiliaze current player to player[0]
+    let currentPlayer = players[0];
     const getCurrentPlayer = () => currentPlayer;
     let gameover = false;
-    let tieGame = false;
     const getGameStatus = () => gameover;
+    let tieGame = false;
     const getTieGameStatus = () => tieGame;
     let board = Gameboard();
     function resetBoard() {
-        board.deleteBoard();
+        board.createBoard();
         gameover = false;
         tieGame = false;
         currentPlayer = players[0];
@@ -52,8 +47,6 @@ function GameController() {
     };
     function checkForWinner() {
         const currentMarker = currentPlayer.marker;
-        console.log("checkrows:" + checkRows() + " checkColumns:" + checkColumns() + " checkDiagonals:" + checkDiagonals());
-
         if (checkRows() || checkColumns() || checkDiagonals()) {
             gameover = true;
         }
@@ -63,7 +56,6 @@ function GameController() {
         function checkRows() {
             for (let row = 0; row < 3; row++) {
                 let boardRow = board.getBoard()[row];
-                console.table("rows: " + boardRow);
                 if (boardRow.every(checkMarkers)) {
                     return true;
                 }
@@ -74,7 +66,6 @@ function GameController() {
         function checkColumns() {
             for (let column = 0; column < 3; column++) {
                 let boardColumn = board.getBoard().map(d => d[column]);
-                console.table("column: " + boardColumn);
                 if (boardColumn.every(checkMarkers)) {
                     return true;
                 }
@@ -87,13 +78,10 @@ function GameController() {
             diagonal1[0] = board.getBoard()[0][0];
             diagonal1[1] = board.getBoard()[1][1];
             diagonal1[2] = board.getBoard()[2][2];
-            console.table("diagonal1: " + diagonal1)
             let diagonal2 = [];
             diagonal2[0] = board.getBoard()[0][2];
             diagonal2[1] = board.getBoard()[1][1];
             diagonal2[2] = board.getBoard()[2][0];
-            console.table("diagonal2: " + diagonal2)
-
             if (diagonal1.every(checkMarkers)) {
                 return true;
             } else if (diagonal2.every(checkMarkers)) {
@@ -101,12 +89,8 @@ function GameController() {
             }
             return false;
         }
-        console.log("gamestatus:" + gameover);
-
     }
     function checkForTie() {
-        console.log("tiegame: " + tieGame);
-
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (board.getBoard()[i][j] === '') {
@@ -174,20 +158,15 @@ function screenController() {
     }
     function checkGameOver() {
         let gameOver = controller.getGameStatus();
-        console.log("gamestatus from checkGame:" + gameOver);
         if (gameOver) {
-            console.log("gameOver statement was true");
             let tieGame = controller.getTieGameStatus();
             if (tieGame) {
-                console.log("Tie game was true");
                 winnerMessage.textContent = "Tie Game!"
             } else {
-                console.log("getting inside else statement");
                 let winner = controller.getCurrentPlayer();
                 winnerMessage.textContent = winner.marker + "'s win!";
             }
             restartBtn.addEventListener("mousedown", restartGame);
-            console.log("its getting to gameOverWindow here");
             gameOverWindow.show();
         }
     }
