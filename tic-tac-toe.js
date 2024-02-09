ba33b98ac92370525e1f48f458e4eee02b1b5d1a
Miclaine Emtman <clainie@gmail.com>
2024-02-09 10:29:37 +0000

    Fix multiple instances of board bug.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,7 +11,8 @@ function Gameboard() {
     }
     const getBoard = () => board;
     const addMarker = (row, column, marker) => board[row][column] = marker;
-    return { getBoard, addMarker };
+    const deleteBoard = () => board = [["", "", ""], ["", "", ""], ["", "", ""]];
+    return { getBoard, addMarker, deleteBoard };
 
 }
 
@@ -32,26 +33,37 @@ function GameController() {
     const getGameStatus = () => gameover;
     const getTieGameStatus = () => tieGame;
     let board = Gameboard();
+    function resetBoard() {
+        board.deleteBoard();
+        gameover = false;
+        tieGame = false;
+        currentPlayer = players[0];
+    }
     function playTurn(row, column) {
         board.addMarker(row - 1, column - 1, currentPlayer.marker);
         console.table(board.getBoard());
         checkForWinner();
-        checkForTie();
+        if (!checkForWinner) {
+            checkForTie();
+        }
     }
     const switchPlayerTurn = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     };
     function checkForWinner() {
         const currentMarker = currentPlayer.marker;
+        console.log("checkrows:" + checkRows() + " checkColumns:" + checkColumns() + " checkDiagonals:" + checkDiagonals());
+
         if (checkRows() || checkColumns() || checkDiagonals()) {
             gameover = true;
-        };
+        }
         function checkMarkers(marker) {
             return marker === currentMarker;
         }
         function checkRows() {
             for (let row = 0; row < 3; row++) {
-                const boardRow = board.getBoard()[row];
+                let boardRow = board.getBoard()[row];
+                console.table("rows: " + boardRow);
                 if (boardRow.every(checkMarkers)) {
                     return true;
                 }
@@ -61,7 +73,8 @@ function GameController() {
 
         function checkColumns() {
             for (let column = 0; column < 3; column++) {
-                const boardColumn = board.getBoard().map(d => d[column]);
+                let boardColumn = board.getBoard().map(d => d[column]);
+                console.table("column: " + boardColumn);
                 if (boardColumn.every(checkMarkers)) {
                     return true;
                 }
@@ -74,34 +87,41 @@ function GameController() {
             diagonal1[0] = board.getBoard()[0][0];
             diagonal1[1] = board.getBoard()[1][1];
             diagonal1[2] = board.getBoard()[2][2];
+            console.table("diagonal1: " + diagonal1)
             let diagonal2 = [];
             diagonal2[0] = board.getBoard()[0][2];
             diagonal2[1] = board.getBoard()[1][1];
             diagonal2[2] = board.getBoard()[2][0];
+            console.table("diagonal2: " + diagonal2)
+
             if (diagonal1.every(checkMarkers)) {
                 return true;
             } else if (diagonal2.every(checkMarkers)) {
                 return true;
             }
+            return false;
         }
+        console.log("gamestatus:" + gameover);
+
     }
     function checkForTie() {
-        const currentBoard = board.getBoard();
+        console.log("tiegame: " + tieGame);
+
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                if (currentBoard[i][j] === '') {
-                    tieGame = true;
+                if (board.getBoard()[i][j] === '') {
                     return false;
                 }
             }
         }
+        tieGame = true;
         gameover = true;
         return true;
     }
-    return { getCurrentPlayer, playTurn, switchPlayerTurn, getGameStatus, getTieGameStatus }
+    return { getCurrentPlayer, playTurn, switchPlayerTurn, getGameStatus, getTieGameStatus, resetBoard }
 }
 
-function displayBoard() {
+function screenController() {
     const gameOverWindow = document.querySelector('dialog');
     const winnerMessage = document.querySelector('#winner-message');
     const restartBtn = document.querySelector('#restart');
@@ -113,14 +133,17 @@ function displayBoard() {
             eval("var row" + i + "col" + j + " = " + "document.querySelector('.row" + i + ".column" + j + "')");
         }
     }
-
-    for (i = 0; i <= cells.length - 1; i++) {
-        cells[i].textContent = '';
-        cells[i].style.color = "gray";
-        cells[i].addEventListener('mouseenter', mouseEnter);
-        cells[i].addEventListener('mouseleave', mouseLeave);
-        cells[i].addEventListener('mousedown', placeMarker);
+    displayBoard();
+    function displayBoard() {
+        for (i = 0; i <= cells.length - 1; i++) {
+            cells[i].textContent = '';
+            cells[i].style.color = "gray";
+            cells[i].addEventListener('mouseenter', mouseEnter);
+            cells[i].addEventListener('mouseleave', mouseLeave);
+            cells[i].addEventListener('mousedown', placeMarker);
+        }
     }
+
     function updateBoard(cell, marker) {
         cell.removeEventListener('mouseenter', mouseEnter);
         cell.removeEventListener('mouseleave', mouseLeave);
@@ -145,23 +168,31 @@ function displayBoard() {
     function restartGame() {
         gameOverWindow.close();
         restartBtn.removeEventListener("mousedown", restartGame);
+        controller.resetBoard();
         displayBoard();
     }
     function checkGameOver() {
         let gameOver = controller.getGameStatus();
+        console.log("gamestatus from checkGame:" + gameOver);
         if (gameOver) {
+            console.log("gameOver statement was true");
             let tieGame = controller.getTieGameStatus();
             if (tieGame) {
+                console.log("Tie game was true");
                 winnerMessage.textContent = "Tie Game!"
             } else {
+                console.log("getting inside else statement");
                 let winner = controller.getCurrentPlayer();
                 winnerMessage.textContent = winner.marker + "'s win!";
             }
-            gameOverWindow.show();
             restartBtn.addEventListener("mousedown", restartGame);
+            console.log("its getting to gameOverWindow here");
+            gameOverWindow.show();
         }
     }
     return { updateBoard }
 };
-
-displayBoard();
+let startBtn = document.querySelector('#start-game');
+startBtn.addEventListener("mousedown", function (event) {
+    screenController();
+});
